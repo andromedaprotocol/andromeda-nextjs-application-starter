2e69ce20160697ee4251f9cbca37df4121d50344
Yves Monem <yves_menhem@outlook.com>
2025-03-21 11:31:47 +0200

    Added mode toggle for dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -6,38 +6,31 @@ import React from "react";
 interface Props {}
 
 const Page = async (props: Props) => {
-    return (
-        <div className="flex min-h-screen items-center justify-center">
-            <div className="flex flex-col items-center space-y-3 text-center">
-                <Image
-                    src="/logo.png"
-                    width={96}
-                    height={96}
-                    alt="Logo"
-                />
-                <h1 className="text-3xl font-bold">
-                    Andromeda Next.js Starter Template
-                </h1>
-                <p>
-                    Click the button to connect <b>Andromeda Devnet</b>.
-                </p>
-                <p className="font-light mb-6">
-                    Learn more about Andromeda&nbsp;
-                    <Link 
-                        href="https://docs.andromedaprotocol.io" 
-                        target="_blank" 
-                        className="text-blue-500 underline"
-                    >
-                        here
-                    </Link>
-                </p>
-                <ConnectWallet />
-                <div className="flex flex-col items-center space-y-3 text-center">
-                    <ModeToggle />
-                </div>
-            </div>
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex flex-col items-center space-y-3 text-center">
+        <Image src="/logo.png" width={96} height={96} alt="Logo" />
+        <h1 className="text-3xl font-bold">Andromeda Nextjs Starter</h1>
+        <p>
+          Click the button to connect <b>Andromeda Devnet</b>.
+        </p>
+        <p className="font-light mb-6">
+          Learn more about Andromeda&nbsp;
+          <Link
+            href="https://docs.andromedaprotocol.io"
+            target="_blank"
+            className="text-blue-500 underline"
+          >
+            here
+          </Link>
+        </p>
+        <ConnectWallet />
+        <div className="flex flex-col items-center space-y-3 text-center">
+          <ModeToggle />
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Page;
