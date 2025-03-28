e5bf3ad30d4113fc17d3a46ff832caf80b0448e7
Yves Monem <yves_menhem@outlook.com>
2025-03-28 16:30:16 +0200

    Update ModeToggle component with dropdown menu

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,20 +1,19 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
-
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 import {
- Button,
+  Button,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/modules/wallet/components/ui"
+} from "@/modules/wallet/components/ui";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
@@ -37,5 +36,5 @@ export function ModeToggle() {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
