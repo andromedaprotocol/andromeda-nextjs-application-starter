f48a0e278d6d008237e05f98fa6601b3244ba2e4
Yves Monem <yves_menhem@outlook.com>
2025-04-02 19:43:27 +0300

    updated tailwind theming

--- src/styles/theme/appTheme.ts
+++ src/styles/theme/appTheme.ts
@@ -1,107 +1,94 @@
+import { COLORS } from "@andromeda/design-tokens/colors";
+import { RADIUS } from "@andromeda/design-tokens/radius";
+import { SIZES } from "@andromeda/design-tokens/sizes";
+import { SPACING } from "@andromeda/design-tokens/spacing";
+import { TYPOGRAPHY } from "@andromeda/design-tokens/typography";
+
 export const APP_THEMES = {
   dark: "dark",
   light: "light",
-} as const;
+};
 
 export type AppTheme = keyof typeof APP_THEMES;
 
 export const appTheme = {
   colors: {
-    border: "hsl(var(--border))",
-    input: "hsl(var(--input))",
-    ring: "hsl(var(--ring))",
-    background: "hsl(var(--background))",
-    foreground: "hsl(var(--foreground))",
+    ...COLORS.colors,
+    border: {
+      DEFAULT: "#958d8d",
+      ...COLORS.colors.border,
+    },
+    input: "#ffffff3d",
+    ring: "#63b3ed",
+    background: {
+      DEFAULT: "#101216",
+      ...COLORS.colors.background,
+    },
+    foreground: "#E0E0E1",
     primary: {
-      DEFAULT: "hsl(var(--primary))",
-      foreground: "hsl(var(--primary-foreground))",
+      ...COLORS.colors.primary,
+      DEFAULT: "#4481FF",
+      foreground: "#FFFFFF",
     },
     secondary: {
-      DEFAULT: "hsl(var(--secondary))",
-      foreground: "hsl(var(--secondary-foreground))",
+      DEFAULT: "#1C1E22",
+      foreground: "#E0E0E1",
     },
     destructive: {
-      DEFAULT: "hsl(var(--destructive))",
-      foreground: "hsl(var(--destructive-foreground))",
+      DEFAULT: "#FE394C",
+      foreground: "#FFFFFF",
     },
     muted: {
-      DEFAULT: "hsl(var(--muted))",
-      foreground: "hsl(var(--muted-foreground))",
+      DEFAULT: "#2D2E32",
+      foreground: "#E0E0E1",
     },
     accent: {
-      DEFAULT: "hsl(var(--accent))",
-      foreground: "hsl(var(--accent-foreground))",
+      DEFAULT: "#f5f4f0",
+      foreground: "black",
     },
     popover: {
-      DEFAULT: "hsl(var(--popover))",
-      foreground: "hsl(var(--popover-foreground))",
+      DEFAULT: "#1C1E22",
+      foreground: "#E0E0E1",
     },
     card: {
-      DEFAULT: "hsl(var(--card))",
-      foreground: "hsl(var(--card-foreground))",
-    },
-    sidebar: {
-      DEFAULT: "var(--color-sidebar)",
-      foreground: "var(--color-sidebar-foreground)",
-      primary: "var(--color-sidebar-primary)",
-      "primary-foreground": "var(--color-sidebar-primary-foreground)",
-      accent: "var(--color-sidebar-accent)",
-      "accent-foreground": "var(--color-sidebar-accent-foreground)",
-      border: "var(--color-sidebar-border)",
-      ring: "var(--color-sidebar-ring)",
+      DEFAULT: "#1C1E22",
+      foreground: "#E0E0E1",
     },
   },
   screens: {
-    sm: "640px",
-    md: "768px",
-    lg: "1024px",
-    xl: "1280px",
-    "2xl": "1536px",
+    ...SIZES.container,
   },
   spacing: {
-    0: "0px",
-    1: "0.25rem",
-    2: "0.5rem",
-    3: "0.75rem",
-    4: "1rem",
-    5: "1.25rem",
-    6: "1.5rem",
-    8: "2rem",
-    10: "2.5rem",
-    12: "3rem",
-    16: "4rem",
-    20: "5rem",
-    24: "6rem",
-    32: "8rem",
+    ...SPACING.spacing,
   },
   fontFamily: {
-    sans: ["var(--font-sans)"],
-    mono: ["var(--font-mono)"],
+    ...TYPOGRAPHY.fontFamilies,
   },
   borderRadius: {
-    none: "0px",
-    sm: "0.125rem",
-    DEFAULT: "0.25rem",
-    md: "0.375rem",
-    lg: "0.5rem",
-    xl: "0.75rem",
-    "2xl": "1rem",
-    "3xl": "1.5rem",
-    full: "9999px",
+    ...RADIUS.raddi,
   },
-  fontSize: {
-    xs: ["0.75rem", { lineHeight: "1rem" }],
-    sm: ["0.875rem", { lineHeight: "1.25rem" }],
-    base: ["1rem", { lineHeight: "1.5rem" }],
-    lg: ["1.125rem", { lineHeight: "1.75rem" }],
-    xl: ["1.25rem", { lineHeight: "1.75rem" }],
-    "2xl": ["1.5rem", { lineHeight: "2rem" }],
-    "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
-    "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
-    "5xl": ["3rem", { lineHeight: "1" }],
-    "6xl": ["3.75rem", { lineHeight: "1" }],
-    "7xl": ["4.5rem", { lineHeight: "1" }],
-    "8xl": ["6rem", { lineHeight: "1" }],
-    "9xl": ["8rem", { lineHeight: "1" }],
-  },
+  fontSize: Object.keys(TYPOGRAPHY.textStyles).reduce(
+    (acc, key) => {
+      const textStyle =
+        TYPOGRAPHY.textStyles[key as keyof typeof TYPOGRAPHY.textStyles];
+      acc[key] = [
+        textStyle.fontSize,
+        {
+          ...textStyle,
+        },
+      ];
+      return acc;
+    },
+    {} as Record<
+      string,
+      [
+        fontSize: string,
+        configuration: Partial<{
+          lineHeight: string;
+          letterSpacing: string;
+          fontWeight: string | number;
+        }>,
+      ]
+    >,
+  ),
 }; 
